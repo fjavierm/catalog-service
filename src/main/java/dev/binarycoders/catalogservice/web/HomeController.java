3c98523f78ddd7aed47905f724fd9093d2e33fd5
fjavierm <fco.javier.marquez@gmail.com>
2023-02-19 18:34:53 +0000

    Add proprety management

---
+++ src/main/java/dev/binarycoders/catalogservice/web/HomeController.java
@@ -0,0 +1,22 @@
+package dev.binarycoders.catalogservice.web;
+
+import dev.binarycoders.catalogservice.config.PolarProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/")
+public class HomeController {
+
+    private final PolarProperties polarProperties;
+
+    public HomeController(PolarProperties polarProperties) {
+        this.polarProperties = polarProperties;
+    }
+
+    @GetMapping
+    public String getGreeting() {
+        return polarProperties.getGreeting();
+    }
+}
